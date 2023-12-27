26513bfb3ac215b434e2c7b509c6d58fe2ce6563
shivam gurunani <namisha.gurunani173261@gmail.com>
2023-12-27 23:11:34 +0530

    Project Completed

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+import Main from './Components/Main/Main.js';
+import Header from './Components/Navbar/Header.js';
+
+function App() {
+  return (
+    <div >
+    <Header/>  
+    <Main/>
+     
+    </div>
+  );
+}
+
+export default App;
